42c0f9597b0587173153b084e223c5b338aadf8e
Alex Stef <alex.stefa+gh@gmail.com>
2013-08-03 14:13:32 +0300

    better formatting

--- spdytest/spdy_compliance.py
+++ spdytest/spdy_compliance.py
@@ -17,19 +17,38 @@ from thor.spdy.frames import enum
 # TerminalColorEscapes contains strings that, when printed to a terminal, will
 # cause subsequent text to be displayed in the given manner.
 Colors = enum(
+    BLACK = r'[0;30m',
     RED = r'[0;31m',
     GREEN = r'[0;32m',
     YELLOW = r'[0;33m',
+    BLUE = r'[0;34m',
+    MAGENTA = r'[0;35m',
+    CYAN = r'[0;36m',
+    WHITE = r'[0;37m',
     BOLD = r'[1m',
+    DIM = r'[2m',
+    BLINK = r'[5m',
     NORMAL = r'[0m')
 
-def setup_logging():
-    log = multiprocessing.log_to_stderr()
-    log.setLevel(logging.INFO)
-    return log
-    
-def wrap(color, str):
-    return color + str + Colors.NORMAL
+def setup_logger():
+    logger = multiprocessing.log_to_stderr()
+    logger.setLevel(logging.DEBUG)
+    return logger
+
+def setup_formatter(use_colors, logger):
+    def _wrapper(level, color, str):
+        if use_colors:
+            logger.log(level, color + str + Colors.NORMAL)
+        else:
+            logger.log(level, str)
+
+    return enum(
+        error = functools.partial(_wrapper, logging.ERROR, Colors.RED),
+        frame = functools.partial(_wrapper, logging.DEBUG, Colors.DIM),
+        exchange = functools.partial(_wrapper, logging.INFO, Colors.YELLOW),
+        session = functools.partial(_wrapper, logging.INFO, Colors.GREEN),
+        status = functools.partial(_wrapper, logging.INFO, Colors.BOLD),
+        notify = functools.partial(_wrapper, logging.INFO, Colors.BLUE))
     
 #-------------------------------------------------------------------------------
 
@@ -61,7 +80,7 @@ class TestRunner(object):
     def __init__(self, settings_file='settings.json'):
         with open(settings_file, 'r') as f:
             self.settings = json.load(f)
-        self.log = setup_logging()
+        self.format = setup_formatter(True, setup_logger())
         self.frame_buff = list()
         
     def setup_session(self, session):
@@ -70,41 +89,47 @@ class TestRunner(object):
         @thor.on(session, 'frame')
         def on_frame(frame):  
             session.frame_buff.append(frame)
-            self.log.info(wrap(Colors.YELLOW, str(frame))) 
+            self.format.frame('SESSION [%s] FRAME %s' % 
+                (session.origin, frame)) 
             
         @thor.on(session, 'error')
         def on_error(error):
-            self.log.info(wrap(Colors.RED, str(error))) 
+            self.format.error('SESSION [%s] ERROR %s' % 
+                (session.origin, error)) 
         
         @thor.on(session, 'bound')
         def on_bound(tcp_conn):
-            self.log.info(wrap(Colors.BOLD, 'BOUND %s' % tcp_conn))
+            self.format.session('SESSION [%s] BOUND' % 
+                session.origin)
             
         @thor.on(session, 'goaway')
         def on_goaway(reason, last_stream_id):
-            self.log.info(wrap(Colors.BOLD, 'GOAWAY %s LSID%d' % 
-                (GoawayReasons.str[reason], last_stream_id)))
+            self.format.session('SESSION [%s] GOAWAY %s LSID=%d' % 
+                (session.origin, GoawayReasons.str[reason], last_stream_id))
         
         @thor.on(session, 'pause')
         def on_pause(paused):
-            self.log.info(wrap(Colors.BOLD, 'PAUSE %s' % paused))
+            self.format.session('SESSION [%s] PAUSE %s' % 
+                (session.origin, paused))
             
         @thor.on(session, 'close')
         def on_close():
-            self.log.info(wrap(Colors.BOLD, 'CLOSED'))
+            self.format.session('SESSION [%s] CLOSED' % 
+                session.origin)
     
     
 class ServerTestRunner(TestRunner):
     def __init__(self, settings_file):
         TestRunner.__init__(self, settings_file)
-        self.log.info(wrap(Colors.RED, 'ServerRunner PID: %s' % os.getpid()))
+        self.format.status('ServerRunner PID: %s' % os.getpid())
         self.cache = FileCache(self.settings['server_webroot'])
         self.server = thor.SpdyServer(
             host=self.settings['server_host'],
             port=self.settings['server_port'],
             idle_timeout=self.settings['connection_idle_timeout'],
             loop=thor.loop.make())
-        self.log.info(wrap(Colors.BOLD, 'LISTENING'))
+        self.format.status('LISTENING AT %s:%d' %
+            (self.server._host, self.server._port))
         self.server.on('session', self.on_session)
         try:
             self.server._loop.run()
@@ -112,52 +137,55 @@ class ServerTestRunner(TestRunner):
             pass
         
     def on_session(self, session):
-        self.log.info(wrap(Colors.BOLD, 'SESSION %s' % session))
+        self.format.session('SESSION NEW %s' % session)
         self.setup_session(session)
         session.on('exchange', self.on_exchange)
     
     def on_exchange(self, exchange):
-        self.log.info(wrap(Colors.GREEN, 'NEW EXCHG %s' % exchange))
+        self.format.exchange('EXCHG NEW %s' % exchange)
         
         @thor.on(exchange, 'error')
         def on_error(error):
-            self.log.info(wrap(Colors.RED, 
-                '[ID=%d] %s' % (exchange.stream_id, error))) 
+            self.format.error('EXCHG [ID=%d] ERROR %s' % 
+                (exchange.stream_id, error))
         
         @thor.on(exchange, 'request_start')
         def on_request_start(hdr_dict):
-            self.log.info(wrap(Colors.NORMAL, 
-                '[ID=%d] REQ_START %s' % (exchange.stream_id, hdr_dict)))
+            self.format.exchange('EXCHG [ID=%d] REQ_START %s' % 
+                (exchange.stream_id, hdr_dict))
             data = self.cache.get_data(hdr_dict.path)
-            exchange.response_start(None, "200 OK")
+            status = "200 OK"
+            self.format.notify('RESPONSE %s %s' % 
+                (status, hdr_dict.uri))
+            exchange.response_start(None, status)
             exchange.response_body(data)
             exchange.response_done()
         
         @thor.on(exchange, 'request_headers')
         def on_request_headers(hdr_dict):
-            self.log.info(wrap(Colors.NORMAL, 
-                '[ID=%d] REQ_HDRS %s' % (exchange.stream_id, hdr_dict))) 
+            self.format.exchange('EXCHG [ID=%d] REQ_HDRS %s' % 
+                (exchange.stream_id, hdr_dict))
         
         @thor.on(exchange, 'request_body')
         def on_request_body(chunk):
-            self.log.info(wrap(Colors.NORMAL, 
-                '[ID=%d] REQ_BODY LEN=%d' % (exchange.stream_id, len(chunk)))) 
+            self.format.exchange('EXCHG [ID=%d] REQ_BODY LEN=%d' % 
+                (exchange.stream_id, len(chunk)))
         
         @thor.on(exchange, 'request_done')
         def on_request_done():
-            self.log.info(wrap(Colors.NORMAL, 
-                '[ID=%d] REQ_DONE' % exchange.stream_id)) 
+            self.format.exchange('EXCHG [ID=%d] REQ_DONE' % 
+                exchange.stream_id)
         
         @thor.on(exchange, 'pause')
         def on_pause(paused):
-            self.log.info(wrap(Colors.NORMAL, 
-                '[ID=%d] PAUSE %s' % (exchange.stream_id, paused))) 
+            self.format.exchange('EXCHG [ID=%d] PAUSE %s' % 
+                (exchange.stream_id, paused))
         
             
 class ClientTestRunner(TestRunner):
     def __init__(self, settings_file):
         TestRunner.__init__(self, settings_file)
-        self.log.info(wrap(Colors.RED, 'ClientRunner PID: %s' % os.getpid()))
+        self.format.status('ClientRunner PID: %s' % os.getpid())
         self.client = thor.SpdyClient(
             connect_timeout=self.settings['client_connect_timeout'],
             read_timeout=self.settings['http_response_timeout'],
@@ -165,55 +193,57 @@ class ClientTestRunner(TestRunner):
             loop=thor.loop.make())
         for entry in self.settings['client_urls']:
             session = self.client.session((entry['host'], entry['port']))
-            self.log.info(wrap(Colors.BOLD, 'SESSION %s' % session))
+            self.format.session('SESSION NEW %s' % session)
             self.setup_session(session)
             for url in  entry['urls']:
-                self.do_request(session, url[0], url[1])
+                self.do_request(session, url[0], url[1], url[2], url[3])
         try:
             self.client._loop.run()
         except KeyboardInterrupt:
             pass
         
-    def do_request(self, session, method, uri):
+    def do_request(self, session, method, uri, headers, body):
         exchange = session.exchange()
-        self.log.info(wrap(Colors.GREEN, 'NEW EXCHG %s' % exchange))
+        self.format.exchange('EXCHG NEW %s' % exchange)
+        self.format.notify('REQUEST %s %s' % (method, uri))
         self.setup_exchange(exchange)
         exchange.request_start(method, uri, None, True)
         
     def setup_exchange(self, exchange):
         @thor.on(exchange, 'error')
         def on_error(error):
-            self.log.info(wrap(Colors.RED, 
-                '[ID=%d] %s' % (exchange.stream_id, error))) 
+            self.format.error('EXCHG [ID=%d] ERROR %s' % 
+                (exchange.stream_id, error))
         
         @thor.on(exchange, 'response_start')
         def on_response_start(hdr_dict):
-            self.log.info(wrap(Colors.NORMAL, 
-                '[ID=%d] RES_START %s' % (exchange.stream_id, hdr_dict)))
+            self.format.exchange('EXCHG [ID=%d] RES_START %s' % 
+                (exchange.stream_id, hdr_dict))
         
         @thor.on(exchange, 'response_headers')
         def on_response_headers(hdr_dict):
-            self.log.info(wrap(Colors.NORMAL, 
-                '[ID=%d] RES_HDRS %s' % (exchange.stream_id, hdr_dict))) 
+            self.format.exchange('EXCHG [ID=%d] RES_HDRS %s' % 
+                (exchange.stream_id, hdr_dict))
         
         @thor.on(exchange, 'response_body')
         def on_response_body(chunk):
-            self.log.info(wrap(Colors.NORMAL, 
-                '[ID=%d] RES_BODY LEN=%d' % (exchange.stream_id, len(chunk)))) 
+            self.format.exchange('EXCHG [ID=%d] RES_BODY LEN=%d' % 
+                (exchange.stream_id, len(chunk)))
         
         @thor.on(exchange, 'response_done')
         def on_response_done():
-            self.log.info(wrap(Colors.NORMAL, 
-                '[ID=%d] RES_DONE' % exchange.stream_id)) 
+            self.format.exchange('EXCHG [ID=%d] RES_DONE' % 
+                exchange.stream_id)
         
         @thor.on(exchange, 'pause')
         def on_pause(paused):
-            self.log.info(wrap(Colors.NORMAL, 
-                '[ID=%d] PAUSE %s' % (exchange.stream_id, paused))) 
+            self.format.exchange('EXCHG [ID=%d] PAUSE %s' % 
+                (exchange.stream_id, paused))
         
         @thor.on(exchange, 'pushed_response')
         def on_pushed_response(pushed_exchange):
-            self.log.info(wrap(Colors.GREEN, 'PUSHED EXCHG %s' % pushed_exchange))
+            self.format.exchange('EXCHG PUSHED %s' % 
+                pushed_exchange)
             self.setup_exchange(pushed_exchange)
         
 #-------------------------------------------------------------------------------
