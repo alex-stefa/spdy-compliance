1674626113f1d7778728f1eef59de26a225bc7bf
Alex Stef <alex.stefa+gh@gmail.com>
2013-09-15 11:54:56 +0300

    directory listing, pushed stream

--- spdytest/spdy_compliance.py
+++ spdytest/spdy_compliance.py
@@ -37,8 +37,9 @@ import time
 import functools
 import socket
 import hashlib
-from urllib.parse import urlsplit, urlunsplit
+from urllib.parse import urlsplit, urlunsplit, quote
 from urllib.request import url2pathname
+from html import escape
 
 import thor
 import thor.spdy.error as spdy_error
@@ -103,7 +104,8 @@ def setup_formatter(logger):
         exchange = functools.partial(_wrapper, logging.INFO, Colors.YELLOW),
         session = functools.partial(_wrapper, logging.INFO, Colors.GREEN),
         status = functools.partial(_wrapper, logging.WARNING, Colors.BOLD),
-        notify = functools.partial(_wrapper, logging.WARNING, Colors.BLUE))
+        notify = functools.partial(_wrapper, logging.WARNING, Colors.BLUE),
+        test = functools.partial(_wrapper, logging.WARNING, Colors.CYAN))
         
 def setup_tls_config(settings):
     return thor.TlsConfig(
@@ -121,42 +123,98 @@ class FileServer(object):
     Should not be used for very large files.
     """
     # TODO: sanitize path (prevent dir traversal)
-    # TODO: directory listing?
     # TODO: implement LRU cache; limit total memory; etc.
     def __init__(self, rootdir='www'):
         self.rootdir = rootdir
     
     @functools.lru_cache(maxsize=64)
-    def _get_file(self, file_path):
+    def get_file(self, file_path):
         try:
             with open(file_path, 'rb') as f:
                 return f.read()
         except OSError:
             return None
-    
-    @staticmethod
-    def _get_err_page(status):
-        return ('<html><body><h1>%s %s</h1></body></html>' % 
-            (str(status[0]), str(status[1]))).encode()
-                
+            
+    @functools.lru_cache(maxsize=64)
+    def get_dir_listing(self, path, base_url):
+        try:
+            list = os.listdir(path)
+        except os.error:
+            return self.get_error((403, 'Forbidden'))
+        list.sort(key=lambda a: a.lower())
+        r = []
+        displaypath = escape(base_url)
+        enc = sys.getfilesystemencoding()
+        title = 'Directory listing for %s' % displaypath
+        r.append('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" '
+                 '"http://www.w3.org/TR/html4/strict.dtd">')
+        r.append('<html>\n<head>')
+        r.append('<meta http-equiv="Content-Type" '
+                 'content="text/html; charset=%s">' % enc)
+        r.append('<title>%s</title>\n</head>' % title)
+        r.append('<body>\n<h1>%s</h1>' % title)
+        r.append('<hr>\n<ul>')
+        upperlink = quote(base_url.rsplit('/', 2)[0] + '/')
+        r.append('<li><a href="%s">[..]</a></li>' % upperlink)
+        for name in list:
+            fullname = os.path.join(path, name)
+            displayname = linkname = name
+            size = '%s bytes' % os.path.getsize(fullname)
+            # Append / for directories or @ for symbolic links
+            if os.path.isdir(fullname):
+                displayname = name + '/'
+                linkname = name + '/'
+                size = 'directory'
+            if os.path.islink(fullname):
+                displayname = name + '@'
+                size = 'link'
+            size = '<i>' + size + '</i>'
+            r.append('<li><a href="%s">%s</a>&nbsp;&nbsp;%s</li>'
+                    % (quote(linkname), escape(displayname), size))
+        r.append('</ul>\n<hr>\n</body>\n</html>\n')
+        encoded = '\n'.join(r).encode(enc)
+        headers = [
+            ('Content-type', 'text/html; charset=%s' % enc),
+            ('Content-Length', str(len(encoded)))]
+        return ((200, 'OK'), encoded, headers)
+
+    ERROR_MESSAGE = """\
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+        "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+        <title>Error response</title>
+    </head>
+    <body>
+        <h1>%s %s</h1>
+    </body>
+</html>
+"""
+
+    def get_error(self, status):
+        return (status,
+            (self.ERROR_MESSAGE % tuple(map(str, status))).encode(
+                'UTF-8', 'replace'),
+            [('Content-type', 'text/html;charset=utf-8')])
+                            
     def handle_request(self, hdr_dict):
         if hdr_dict.method.lower() != 'get':
-            status = (501, 'Not Implemented')
-            return (status, self._get_err_page(status))
+            return self.get_error((501, 'Not Implemented'))
         path = url2pathname(hdr_dict.path_split[0])
         if path[0] != '/':
-            status = (400, 'Bad Request') 
-            return (status, self._get_err_page(status))
+            return self.get_error((400, 'Bad Request'))
         path = path[1:]
         file_path = os.path.join(self.rootdir, path)
         if os.path.isdir(file_path):
-            status = (403, 'Forbidden')
-            return (status, self._get_err_page(status))
-        data = self._get_file(file_path)
+            base_url = '/' + path
+            if base_url[-1] != '/':
+                base_url += '/'
+            return self.get_dir_listing(file_path, base_url)
+        data = self.get_file(file_path)
         if data is None:
-            status = (404, 'Not Found')
-            return (status, self._get_err_page(status))
-        return ((200, 'OK'), data)
+            return self.get_error((404, 'Not Found'))
+        return ((200, 'OK'), data, [('Content-Length', str(len(data)))])
 
 #-------------------------------------------------------------------------------
 
@@ -227,12 +285,12 @@ class SimpleDNS(multiprocessing.Process):
         self.dns_port = settings['dns_port']
         self.reply_ip = settings['dns_A_reply']
         
-    def process_request(data):
+    def process_request(self, data):
         domain = b''
         type = (data[2] >> 3) & 15 # Opcode bits
         if type == 0: # Standard query
             start = 12
-            size = data[ini]
+            size = data[start]
         else:
             self.format.notify('Unsupported request type: %d' % type)
             return (None, None)
@@ -268,7 +326,7 @@ class SimpleDNS(multiprocessing.Process):
         try:
             while True:
                 (data, address) = udps.recvfrom(1024)
-                (domain, reply) = process_request(data)
+                (domain, reply) = self.process_request(data)
                 if reply:
                     self.format.notify('Replied to %s with %s for query %s.' % (
                         address, self.reply_ip, domain))
@@ -278,8 +336,14 @@ class SimpleDNS(multiprocessing.Process):
 
 #-------------------------------------------------------------------------------
     
-class TestRunner(multiprocessing.Process):
+class SpdyRunner(multiprocessing.Process):
+    _base_name = 'SPDY'
+    _instances = 0
+    
     def __init__(self, settings, name=None):
+        type(self)._instances += 1
+        if not name:
+            name = '%s-%d' % (self._base_name, self._instances)
         multiprocessing.Process.__init__(self, name=name)
         self.settings = settings
         self.tls_config = None
@@ -341,10 +405,14 @@ class TestRunner(multiprocessing.Process):
             self.format.session('SESSION [%s] CLOSED' % 
                 session.origin)
     
+#-------------------------------------------------------------------------------
+
+class ServerRunner(SpdyRunner):
+    _base_name = 'SERVER'
+    _instances = 0
     
-class ServerTestRunner(TestRunner):
-    def __init__(self, settings, name='SERVER'):
-        TestRunner.__init__(self, settings, name)
+    def __init__(self, settings, name=None):
+        SpdyRunner.__init__(self, settings, name)
         if self.settings['server_use_spdy_advertiser']:
             self.padv = SpdyAdvertiser(self.settings)
             self.padv.start()   
@@ -384,12 +452,12 @@ class ServerTestRunner(TestRunner):
         
         @thor.on(exchange, 'error')
         def on_error(error):
-            self.format.error('EXCHG [ID=%d] ERROR %s' % 
+            self.format.error('EXCHG [ID=%s] ERROR %s' % 
                 (exchange.stream_id, error))
         
         @thor.on(exchange, 'request_start')
         def on_request_start(hdr_dict):
-            self.format.exchange('EXCHG [ID=%d] REQ_START %s' % 
+            self.format.exchange('EXCHG [ID=%s] REQ_START %s' % 
                 (exchange.stream_id, hdr_dict))
             exchange.req_hdrs = hdr_dict
             exchange.req_body = b''
@@ -398,31 +466,60 @@ class ServerTestRunner(TestRunner):
         
         @thor.on(exchange, 'request_headers')
         def on_request_headers(hdr_dict):
-            self.format.exchange('EXCHG [ID=%d] REQ_HDRS %s' % 
+            self.format.exchange('EXCHG [ID=%s] REQ_HDRS %s' % 
                 (exchange.stream_id, hdr_dict))
             exchange.req_hdrs.update(hdr_dict)
         
         @thor.on(exchange, 'request_body')
         def on_request_body(chunk):
-            self.format.exchange('EXCHG [ID=%d] REQ_BODY LEN=%d' % 
+            self.format.exchange('EXCHG [ID=%s] REQ_BODY LEN=%d' % 
                 (exchange.stream_id, len(chunk)))
             exchange.req_body += chunk
         
         @thor.on(exchange, 'request_done')
         def on_request_done():
-            self.format.exchange('EXCHG [ID=%d] REQ_DONE' % 
+            self.format.exchange('EXCHG [ID=%s] REQ_DONE' % 
                 exchange.stream_id)
             if exchange.is_local:
-                (res_status, res_body) = self.local_server.handle_request(
-                    exchange.req_hdrs) 
+                (res_status, res_body, res_hdrs) = \
+                    self.local_server.handle_request(exchange.req_hdrs)
                 self.format.notify('RESPONSE %s %s' % 
                     (res_status, exchange.req_hdrs.uri))
-                if res_body:
-                    exchange.response_start(None, res_status)
-                    exchange.response_body(res_body)
-                    exchange.response_done()
-                else:
-                    exchange.response_start(None, res_status, done=True)
+                exchange.response_start(res_hdrs, res_status)
+                exchange.response_body(res_body)
+                if res_status[1] == 'OK':
+                    req_path = url2pathname(exchange.req_hdrs.path_split[0])[1:]
+                    push_paths = self.settings['server_push_map'].get(
+                        req_path, []) 
+                    for (push_path, push_priority) in push_paths:
+                        file_path = os.path.join(self.local_server.rootdir, 
+                            push_path)
+                        push_body = self.local_server.get_file(file_path)
+                        if push_body is None:
+                            self.format.error('Could not read pushed resource'
+                                ' %s associated to request %s' %
+                                (file_path, exchange.req_hdrs.uri))
+                        else:
+                            push_exchg = exchange.push_response()
+                            push_exchg.priority = push_priority
+                            
+                            @thor.on(push_exchg, 'error')
+                            def on_push_error(error):
+                                self.format.error('EXCHG [ID=%s] ERROR %s' % 
+                                    (exchange.stream_id, error))
+                            
+                            self.format.exchange('EXCHG PUSHED %s' % 
+                                push_exchg)
+                            push_uri = urlunsplit((
+                                exchange.req_hdrs.scheme,
+                                exchange.req_hdrs.authority,
+                                quote(push_path), '', ''))
+                            self.format.notify('PUSH RESPONSE %s for %s' % 
+                                (file_path, exchange.req_hdrs.uri))
+                            push_exchg.response_start(None, uri=push_uri)
+                            push_exchg.response_body(push_body)
+                            push_exchg.response_done()
+                exchange.response_done()
             else:
                 if not self.settings['server_use_proxy']:
                     res_status = (503, 'Service unavailable')
@@ -455,10 +552,10 @@ class ServerTestRunner(TestRunner):
                         
                     @thor.on(http_exchg, 'error')
                     def on_http_error(error):
-                        self.format.notify('Failed to fetch %s %s: %s' % (
+                        self.format.notify('Failed to fetch %s %s: %r' % (
                             exchange.req_hdrs.method, 
                             exchange.req_hdrs.uri, 
-                            repr(error)))
+                            error))
                         res_status = (500, 'Internal Server Error')
                         self.format.notify('RESPONSE %s %s ' % 
                             (res_status, exchange.req_hdrs.uri))
@@ -478,13 +575,17 @@ class ServerTestRunner(TestRunner):
         
         @thor.on(exchange, 'pause')
         def on_pause(paused):
-            self.format.exchange('EXCHG [ID=%d] PAUSE %s' % 
+            self.format.exchange('EXCHG [ID=%s] PAUSE %s' % 
                 (exchange.stream_id, paused))
         
+#-------------------------------------------------------------------------------
             
-class ClientTestRunner(TestRunner):
-    def __init__(self, settings, name='CLIENT'):
-        TestRunner.__init__(self, settings, name)
+class ClientRunner(SpdyRunner):
+    _base_name = 'CLIENT'
+    _instances = 0
+    
+    def __init__(self, settings, name=None):
+        SpdyRunner.__init__(self, settings, name)
         
     def setup(self):
         self.format.status('ClientRunner PID: %s' % os.getpid())
@@ -497,6 +598,8 @@ class ClientTestRunner(TestRunner):
             tls_config=self.tls_config,
             loop=self.loop)
         for entry in self.settings['client_urls']:
+            if not entry["enabled"]:
+                continue
             session = self.client.session((entry['host'], entry['port']))
             self.format.session('SESSION NEW %s' % session)
             self.setup_session(session)
@@ -505,6 +608,11 @@ class ClientTestRunner(TestRunner):
         
     def do_request(self, session, method, uri, headers, body, checksum):
         exchange = session.exchange()
+        # NOTE: this is a basic check; we expect quoted URLs in the settings file
+        if ' ' in uri:
+            parts = urlsplit(uri)
+            uri = urlunsplit((parts[0], parts[1], quote(parts[2]), 
+                quote(parts[3]), quote(parts[4])))
         self.format.notify('REQUEST %s %s' % (method, uri))
         self.setup_exchange(exchange)
         exchange.checksum = checksum
@@ -525,22 +633,26 @@ class ClientTestRunner(TestRunner):
     def setup_exchange(self, exchange):
         @thor.on(exchange, 'error')
         def on_error(error):
-            self.format.error('EXCHG [ID=%d] ERROR %s' % 
+            self.format.error('EXCHG [ID=%s] ERROR %s' % 
                 (exchange.stream_id, error))
         
         @thor.on(exchange, 'response_start')
         def on_response_start(hdr_dict):
-            self.format.exchange('EXCHG [ID=%d] RES_START %s' % 
+            self.format.exchange('EXCHG [ID=%s] RES_START %s' % 
                 (exchange.stream_id, hdr_dict))
+            if exchange._pushed:
+                exchange.checksum = None
+                exchange.hasher = hashlib.md5()
+                exchange.uri = hdr_dict.uri
         
         @thor.on(exchange, 'response_headers')
         def on_response_headers(hdr_dict):
-            self.format.exchange('EXCHG [ID=%d] RES_HDRS %s' % 
+            self.format.exchange('EXCHG [ID=%s] RES_HDRS %s' % 
                 (exchange.stream_id, hdr_dict))
         
         @thor.on(exchange, 'response_body')
         def on_response_body(chunk):
-            self.format.exchange('EXCHG [ID=%d] RES_BODY LEN=%d' % 
+            self.format.exchange('EXCHG [ID=%s] RES_BODY LEN=%d' % 
                 (exchange.stream_id, len(chunk)))
             exchange.hasher.update(chunk)
         
@@ -549,23 +661,23 @@ class ClientTestRunner(TestRunner):
             digest = exchange.hasher.hexdigest()
             if exchange.checksum:
                 if exchange.checksum == digest:
-                    self.format.exchange('EXCHG [ID=%d] RES_DONE'
+                    self.format.exchange('EXCHG [ID=%s] RES_DONE'
                         ' [MD5 MATCH: %s]' % 
                         (exchange.stream_id, exchange.checksum))
                 else:
-                    self.format.exchange('EXCHG [ID=%d] RES_DONE'
+                    self.format.exchange('EXCHG [ID=%s] RES_DONE'
                         ' [MD5 MISMATCH: GOT=%s, EXPECTED=%s]' % 
                         (exchange.stream_id, digest, exchange.checksum))
                     self.format.error('MD5 digest mismatch for %s:'
                         '\n\t%s computed\n\t%s expected' % 
                         (exchange.uri, digest, exchange.checksum))
             else:
-                self.format.exchange('EXCHG [ID=%d] RES_DONE'
+                self.format.exchange('EXCHG [ID=%s] RES_DONE'
                     ' [MD5: %s]' % (exchange.stream_id, digest))
         
         @thor.on(exchange, 'pause')
         def on_pause(paused):
-            self.format.exchange('EXCHG [ID=%d] PAUSE %s' % 
+            self.format.exchange('EXCHG [ID=%s] PAUSE %s' % 
                 (exchange.stream_id, paused))
         
         @thor.on(exchange, 'pushed_response')
@@ -574,6 +686,24 @@ class ClientTestRunner(TestRunner):
                 pushed_exchange)
             self.setup_exchange(pushed_exchange)
         
+#-------------------------------------------------------------------------------
+
+class ServerTestRunner(SpdyRunner):
+    _base_name = 'SERVER-TEST'
+    _instances = 0
+
+    def __init__(self, settings, name=None):
+        SpdyRunner.__init__(self, settings, name)
+
+#-------------------------------------------------------------------------------
+
+class ClientTestRunner(SpdyRunner):
+    _base_name = 'CLIENT-TEST'
+    _instances = 0
+
+    def __init__(self, settings, name=None):
+        SpdyRunner.__init__(self, settings, name)
+
 #-------------------------------------------------------------------------------
 
 if __name__ == "__main__":
@@ -591,14 +721,20 @@ Usage:
     if settings['log_file_level'] and settings['log_file_dir']:
         os.makedirs(settings['log_file_dir'], exist_ok=True)
     procs = list()
-    if settings['mode'] in ['server', 'mixed']:
-        p = ServerTestRunner(settings)
+    if settings['server_enabled']:
+        if settings['server_test_mode']:
+            p = ServerTestRunner(settings, name='SERVER-TEST')
+        else:
+            p = ServerRunner(settings, name='SERVER')
         procs.append(p)
         p.start()
         time.sleep(2) # leave a little time to set up server socket
-    if settings['mode'] in ['client', 'mixed']:
-        for i in range(settings['client_count']):
-            p = ClientTestRunner(settings, 'CLIENT-%d' % i)
+    if settings['client_enabled']:
+        for i in range(settings['client_instances']):
+            if settings['client_test_mode']:
+                p = ClientTestRunner(settings, name='CLIENT-TEST-%d' % (i+1))
+            else:
+                p = ClientRunner(settings, name='CLIENT-%d' % (i+1))
             procs.append(p)
             p.start()
     try:
